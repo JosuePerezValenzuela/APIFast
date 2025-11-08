7f21970df89482b994f45033c341cc9f236d1c2d
Josue Perez <josueperezv2004@gmail.com>
2025-11-07 22:35:09 -0400

    Metodos post con request Body

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from enum import Enum
 
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
@@ -82,3 +84,18 @@ async def read_user_item(
             {"description": "This is an amazing item that has a long description"}
         )
     return item
+
+class Item (BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+@app.post("/items/{item_id}")
+async def create_item(item_id: int, item: Item):
+    item_dict = item.model_dump()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    item_dict.update({"item_id": item_id})
+    return item_dict
