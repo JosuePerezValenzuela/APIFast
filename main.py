d90dcadd754f5eb3b0b800b8ab7633c877b46c43
Josue Perez <josueperezv2004@gmail.com>
2025-11-07 21:41:44 -0400

    Multiples parametros de ruta y query

--- main.py
+++ main.py
@@ -67,3 +67,18 @@ async def read_item3(item_id: str, q: str | None = None, short: bool = False):
             {"description": "This is an amazing item that has a long description"}
         )
     return item
+
+# ** Multiples parametros de ruta y query
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+        user_id: int, item_id: str, needy: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id, "needy": needy}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
